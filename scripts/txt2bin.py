fa3aaa9d32c00021823ea962cc325a204144fe4b
Yuhao Gu <yhgu2000@outlook.com>
2023-05-29 23:07:06 +0800

    修订txt2bin脚本
    原来的有错误，且无法处理大文件

--- scripts/txt2bin.py
+++ scripts/txt2bin.py
@@ -5,34 +5,33 @@ if len(argv) < 3:
     print("Arguments: <input_txt_file> <output_bin_file> [float]")
     exit(1)
 
-a = 0  # 记录文件的行数
-b = 0  # 记录文件的列数
-data_list = []  # 保存文件数据的一维数组
 filename = argv[1]
 binname = argv[2]
 
-# 打开文件并读取数据
-with open(filename, "r") as f:
-    for line in f:
-        # 按逗号分隔并转换为浮点数类型
-        line_list = [float(num) for num in line.split(",")]
-        data_list += line_list
-
-        # 更新行数和列数
-        a += 1
-        if b == 0:
-            b = len(line_list)
-
-# 将行数和列数打包为8字节的二进制数据
-header = struct.pack("<ii", b, a)
-
-# 将一维数组打包为二进制数据
 if len(argv) > 3 and argv[3] == "float":
-    data_bytes = struct.pack("<{}f".format(a * b), *data_list)
+    packstr = "<{}f"
 else:
-    data_bytes = struct.pack("<{}d".format(a * b), *data_list)
+    packstr = "<{}d"
 
-# 将二进制数据写入文件
-with open(binname, "wb") as f:
-    f.write(header)
-    f.write(data_bytes)
+# 打开文件并读取数据
+a = 0  # 记录文件的行数
+b = None  # 记录文件的列数
+with open(filename, "r") as f:
+    with open(binname, "wb") as fb:
+        fb.seek(8)
+        for line in f:
+            # 按逗号分隔并转换为浮点数类型
+            line_list = [float(num) for num in line.split(",")]
+            data_bytes = struct.pack(packstr.format(len(line_list)), *line_list)
+            fb.write(data_bytes)
+
+            # 更新行数和列数
+            a += 1
+            if b is None:
+                b = len(line_list)
+            else:
+                assert b == len(line_list)
+        fb.flush()
+        fb.seek(0)
+        fb.write(struct.pack("<ii", b, a))
+
